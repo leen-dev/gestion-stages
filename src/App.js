568fceb28406aca0cb844734a1952f5ff649f139
Katy EKOUE <ekouekatygracia@gmail.com>
2025-04-23 15:02:46 +0200

    arborescence et creation des pages et des routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import AppRoutes from "../routes/appRoutes";  
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/a-propos" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
-  );
+  return <AppRoutes />;
 }
-
 export default App;
